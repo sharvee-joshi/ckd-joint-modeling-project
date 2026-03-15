d9a8b38606e1d92d61d07867a81fe39f2d8ae6f2
Sharvee Joshi <sharvee.a.joshi@gmail.com>
2026-03-14 17:12:47 -0700

    Add files via upload
    added libraries set up

---
+++ 00_libraries.R
@@ -0,0 +1,17 @@
+#Load all required packages for the CKD project
+
+packages <- c(
+  "dplyr",
+  "ggplot2",
+  "survival",
+  "survminer",
+  "kableExtra"
+)
+
+install_if_missing <- function(pkg){
+  if(!pkg %in% installed.packages()){
+    install.packages(pkg)
+  }
+}
+
+sapply(packages, install_if_missing)
